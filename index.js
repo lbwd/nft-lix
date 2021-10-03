47cc6479c9d2659a77f412ee5ee26a795c42cdaa
lbwd <lucaunliked@gmail.com>
2021-10-03 19:04:59 +0200

    First try of titlelayout test.

---
+++ index.js
@@ -0,0 +1,27 @@
+var wordType;
+(function (wordType) {
+    wordType[wordType["Noun"] = 0] = "Noun";
+    wordType[wordType["Verb"] = 1] = "Verb";
+    wordType[wordType["Adjective"] = 2] = "Adjective";
+    wordType[wordType["FromArray"] = 3] = "FromArray";
+})(wordType || (wordType = {}));
+// Test objs
+var testWord = {
+    type: wordType.Noun,
+    subType: '',
+    value: 'Employer',
+    article: true
+};
+var testLayout = {
+    layout: '{0}',
+    words: [{ word: testWord }]
+};
+// Functions
+function compileTitle(title) {
+    title.value = title.layout;
+    var re = new RegExp('{(.)}', 'i');
+    title.value = title.value.replace(re, title.words['$1'].word.value);
+    return title;
+}
+// Main
+console.log(compileTitle(testLayout).value);
--- index.ts
+++ index.ts
@@ -1,3 +1,51 @@
-type titleLayout = {};
+// Types
+type titleLayout = {
+	layout: string;
+	words: titleWord[];
+	value?: string;
+};
 
-console.log('test');
+type word = {
+	type: wordType;
+	subType?: string;
+	value: string;
+	randomFrom?: string[];
+	article: boolean;
+};
+
+type titleWord = {
+	word: word;
+	properties?: object[];
+};
+
+enum wordType {
+	Noun,
+	Verb,
+	Adjective,
+	FromArray,
+}
+
+// Test objs
+let testWord: word = {
+	type: wordType.Noun,
+	subType: '',
+	value: 'Employer',
+	article: true,
+};
+
+let testLayout: titleLayout = {
+	layout: '{0}',
+	words: [{ word: testWord }],
+};
+
+// Functions
+function compileTitle(title: titleLayout) {
+	title.value = title.layout;
+	let re = new RegExp('{(.)}', 'i');
+	title.value = title.value.replace(re, title.words['$1'].word.value);
+
+	return title;
+}
+
+// Main
+console.log(compileTitle(testLayout).value);
