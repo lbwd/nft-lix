76145949c5cd5502f141b7556bc36dc763262b8d
lbwd <lucaunliked@gmail.com>
2021-10-05 13:06:12 +0200

    Bind port for heroku.

--- app.ts
+++ app.ts
@@ -3,7 +3,7 @@ import { main } from "./functions";
 import { Title } from "./types";
 
 const app = express();
-const port = 3000;
+const port = process.env.PORT || 3000;
 
 app.listen(port, () => {
 	console.log("App listening on port " + port);
